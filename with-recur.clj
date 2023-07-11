a2dd0ebe473da72c774bb6180808342fa3383651
Carht <carht@protonmail.com>
2023-07-11 12:05:53 -0400

    Adding range using recur.

--- with-recur.clj
+++ with-recur.clj
@@ -16,3 +16,16 @@
       (recur (rest i-list) (inc acc)))))
 
 (my-length '(1 2 3 4))
+
+
+(defn my-range [start stop step]
+  (loop [a start
+         b stop
+         c step
+         acc []]
+    (if (>= a b)
+      acc
+      (recur (+ a c) b c (conj acc (+ a c))))))
+
+(my-range 0 5 1)
+(my-range 0 10 2)
